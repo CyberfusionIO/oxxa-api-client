dad3f5213bd986ce026ab1294bc9d41cb00e94ad
Dick van der Heiden <dick@goedemiddag.nl>
2024-09-19 12:09:31 +0200

    Bump version

--- src/Oxxa.php
+++ src/Oxxa.php
@@ -15,7 +15,7 @@ class Oxxa implements OxxaClient
 {
     private const TIMEOUT = 180;
 
-    private const VERSION = '2.8.1';
+    private const VERSION = '2.9.0';
 
     private const USER_AGENT = 'oxxa-api-client/'.self::VERSION;
 
